d135a85ef08c8e1f555fbe26c9e0395cfa277f11
Antoine Cahard <57390935+Drahac@users.noreply.github.com>
2023-01-05 10:52:11 +0100

    Life gradient

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,6 +7,9 @@ public class HealthBar : MonoBehaviour
 {
     private Slider slider;
 
+    [SerializeField] private Gradient gradient;
+    [SerializeField] private Image fill;
+
     void Awake()
     {
         slider = GetComponent<Slider>();
@@ -16,10 +19,14 @@ public class HealthBar : MonoBehaviour
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+
+        fill.color= gradient.Evaluate(slider.normalizedValue);
     }
 }
