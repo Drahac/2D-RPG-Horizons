19780298d6329daec2072ed3523c09c7fa4bd900
Antoine Cahard <57390935+Drahac@users.noreply.github.com>
2023-01-05 14:58:03 +0100

    2nd level et transition

---
+++ Assets/Scripts/DontDestroyOnLoadScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DontDestroyOnLoadScene : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject[] objects;
+
+    void Awake()
+    {
+        foreach (var obj in objects)
+        {
+            DontDestroyOnLoad(obj);
+        }
+    }
+
+}
