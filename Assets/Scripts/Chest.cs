57c8762dd1cb502bf74f1e3f5173ecc8861bf6f6
Antoine Cahard <57390935+Drahac@users.noreply.github.com>
2023-01-18 16:53:24 +0100

    Chest v0.1

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private bool isOpen =false;
+
+    private Animator animator;
+
+    [SerializeField] AudioClip sound;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OpenChest();
+        }
+    }
+
+    private void OpenChest()
+    {
+        if (!isOpen)
+        {
+            animator.SetTrigger("OpenChest");
+            isOpen = true;
+            AudioManager.Instance.PlayClipAt(sound, transform.position);
+        }
+    }
+}
